f78cc0ba8216b65153806e1e0db1fa9957ae4267
Jules010209 <67187964+Jules010209@users.noreply.github.com>
2022-11-29 21:49:48 +0100

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { ApiService } from './system/controllers/api/api.service';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { ConnexionService } from './system/controllers/connexion/connexion.service';
+import { UserService } from './system/controllers/user/user.service';
 
 @Module({
   imports: [SystemModule],
   controllers: [AppController],
-  providers: [ApiService, AppService, ConnexionService],
+  providers: [ApiService, AppService, ConnexionService, UserService],
 })
 
 export class AppModule {}
